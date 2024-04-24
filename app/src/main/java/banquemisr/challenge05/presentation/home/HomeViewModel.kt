24512988283b5f3a4e2fe212f6bfb18df4d921af
Omneya Osman <engomneya.osman@gmail.com>
2024-04-24 16:27:39 +0200

    testing viewModel

--- app/src/main/java/banquemisr/challenge05/presentation/home/HomeViewModel.kt
+++ app/src/main/java/banquemisr/challenge05/presentation/home/HomeViewModel.kt
@@ -3,12 +3,14 @@ package banquemisr.challenge05.presentation.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import banquemisr.challenge05.domain.model.Movie
-import banquemisr.challenge05.domain.useCase.GetMovieDetailsUseCase
 import banquemisr.challenge05.domain.useCase.HomeUseCases
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
@@ -16,9 +18,20 @@ import javax.inject.Inject
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val homeUseCases: HomeUseCases,
-    detailsUseCase: GetMovieDetailsUseCase
+//    detailsUseCase: GetMovieDetailsUseCase
 ) : ViewModel() {
 
-    val nowPlayingMovieList : StateFlow<PagingData<Movie>> = homeUseCases.getNowPlayingMovieUseCase.invoke()
-        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), PagingData.empty())
+    val nowPlayingMovieList: StateFlow<PagingData<Movie>> =
+        homeUseCases.getNowPlayingMovieUseCase.invoke()
+            .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), PagingData.empty())
+
+    val nowPlayingMovieListt =
+        homeUseCases.getNowPlayingMovieUseCase.invoke().flowOn(Dispatchers.IO)
+            .cachedIn(viewModelScope)
+//            .stateIn(viewModelScope, SharingStarted.WhileSubscribed(), PagingData.empty())
+//
+//            .flowOn(Dispatchers.IO)
+//            .cachedIn(viewModelScope)
+
+
 }
