cea2faa6a0f72db135441c2cbb85aa2c9830546c
Omneya Osman <engomneya.osman@gmail.com>
2024-04-30 01:28:04 +0300

    reallocate fakeData TestData class

--- app/src/test/java/banquemisr/challenge05/presentation/HomeViewModelTest.kt
+++ app/src/test/java/banquemisr/challenge05/presentation/HomeViewModelTest.kt
@@ -6,8 +6,7 @@ import app.cash.turbine.test
 import banquemisr.challenge05.data.entities.MovieEntity
 import banquemisr.challenge05.data.repo.MoviesRepositoryImp
 import banquemisr.challenge05.data.utils.MainCoroutinesRule
-import banquemisr.challenge05.domain.model.Genre
-import banquemisr.challenge05.domain.model.Movie
+import banquemisr.challenge05.data.utils.TestData.resultList
 import banquemisr.challenge05.domain.useCase.GetNowPlayingMoviesUseCase
 import banquemisr.challenge05.domain.useCase.GetPopularMoviesUseCase
 import banquemisr.challenge05.domain.useCase.GetUpComingMoviesUseCase
@@ -126,33 +125,4 @@ class HomeViewModelTest {
     }
 
 
-
-    private val resultList = listOf(
-        Movie(
-            929590,
-            "/uv2twFGMk2qBdyJBJAVcrpRtSa9.jpg",
-            listOf(Genre(0, "genre 1")),
-            "en",
-            "Civil War",
-            "In the near future, a group of war journalists attempt to survive while reporting the truth as the United States stands on the brink of civil war.",
-            532.705,
-            "/sh7Rg8Er3tFcN9BpKIPOMvALgZd.jpg",
-            "2024-04-10", 0,
-            "Civil War",
-            7.404,
-            120, toString()
-        ), Movie(
-            823464, "/j3Z3XktmWB1VhsS8iXNcrR86PXi.jpg",
-            listOf(Genre(0, "genre")),
-            "en",
-            "Godzilla x Kong: The New Empire",
-            "Following their explosive showdown, Godzilla and Kong must reunite against a colossal undiscovered threat hidden within our world, challenging their very existence – and our own.",
-            1851.749,
-            "/tMefBSflR6PGQLv7WvFPpKLZkyk.jpg",
-            "2024-03-27", 0,
-            "Godzilla x Kong: The New Empire",
-            6.699,
-            654, toString()
-        )
-    )
 }
