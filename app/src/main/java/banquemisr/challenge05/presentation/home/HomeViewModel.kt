c3d7d5579704ae649adade731b8a06fd305ee344
Omneya Osman <engomneya.osman@gmail.com>
2024-04-29 21:38:34 +0300

    remove val

--- app/src/main/java/banquemisr/challenge05/presentation/home/HomeViewModel.kt
+++ app/src/main/java/banquemisr/challenge05/presentation/home/HomeViewModel.kt
@@ -12,7 +12,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val homeUseCases: HomeUseCases,
+    homeUseCases: HomeUseCases,
 ) : ViewModel() {
 
     val nowPlayingMovies =
